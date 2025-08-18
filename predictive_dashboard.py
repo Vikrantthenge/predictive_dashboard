44ef679a0ed4bd5e5f45ef6e4edf1885c4bf742a
VIKRANT THENGE <90523497+Vikrantthenge@users.noreply.github.com>
2025-08-18 22:34:09 +0530

    Add files via upload

--- predictive_dashboard.py
+++ predictive_dashboard.py
@@ -6,7 +6,7 @@ import streamlit as st
 st.markdown(
     """
     <div style='text-align: center'>
-        <img src='https://raw.githubusercontent.com/Vikrantthenge/predictive_dashboard/main/predictive_dashboard_banner.png' width='400'/>
+        <img src='https://raw.githubusercontent.com/Vikrantthenge/predictive_dashboard/main/predictive_dashboard_banner.png' width='200'/>
     </div>
     """,
     unsafe_allow_html=True
