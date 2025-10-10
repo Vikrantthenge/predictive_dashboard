d532784c37fba1c47aa4ccbb4f494978ffb05a8d
VIKRANT THENGE <90523497+Vikrantthenge@users.noreply.github.com>
2025-10-10 16:35:03 +0530

    Add files via upload

--- predictive_dashboard.py
+++ predictive_dashboard.py
@@ -283,6 +283,17 @@ st.markdown("""
 <div style='text-align: center; font-size: 16px; padding-top: 10px;'>
     <span style='font-size: 18px;'>🔧 <strong>Built by Vikrant Thenge</strong></span><br>
     <span style='color: #888;'>📊 Data Analyst | 🤖 Automation Strategist | 🎯 Recruiter-Ready Dashboards</span><br><br>
-    <a href='mailto:vikrant.thenge@gmail.com' style='text-decoration: none; font-weight: bold;'>📫 Contact</a>
+
+    <a href='mailto:vikrantthenge@outlook.com' style='text-decoration: none; margin: 0 10px;'>
+        <img src='https://img.icons8.com/color/48/000000/microsoft-outlook.png' alt='Outlook' width='28'/> 
+    </a>
+
+    <a href='https://github.com/Vikrantthenge' target='_blank' style='text-decoration: none; margin: 0 10px;'>
+        <img src='https://img.icons8.com/ios-glyphs/30/000000/github.png' alt='GitHub' width='28'/>
+    </a>
+
+    <a href='https://www.linkedin.com/in/vthenge/' target='_blank' style='text-decoration: none; margin: 0 10px;'>
+        <img src='https://img.icons8.com/color/48/000000/linkedin.png' alt='LinkedIn' width='28'/>
+    </a>
 </div>
 """, unsafe_allow_html=True)
